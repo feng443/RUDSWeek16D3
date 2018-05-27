9ea7d363fe80c4c1325566ecf4cb5ca856179eb8
Chan Feng <feng443@gmail.com>
2018-05-27 19:53:38 -0400

    Done with more data and switching fact/risk

--- Skeleton/assets/js/app.js
+++ Skeleton/assets/js/app.js
@@ -3,6 +3,24 @@
 // Students:
 // =========
 // Follow your written instructions and create a scatter plot with D3.js.
+
+
+const FACTS = {
+    poverty: 'In Poverty (%)',
+    age: 'Age (Median)',
+    income: 'Household Income (Median',
+}
+
+const RISKS = {
+    healthcare: 'Lack of Healthcare (%)',
+    smoke: 'Smokes (%)',
+    obese: 'Obesse (%)',
+}
+
+// Global variable to save current FACT and RISK
+var FACT = Object.keys(FACTS)[0]
+var RISK = Object.keys(RISKS)[0]
+
 d3.select(window).on('resize', makeResponsive)
 
 makeResponsive()
@@ -12,14 +30,14 @@ function makeResponsive() {
 
     if (!svgArea.empty()) svgArea.remove()
 
-    var svgWidth = window.innerWidth
-    var svgHeight = window.innerHeight
+    var svgWidth = window.innerWidth - 10
+    var svgHeight = window.innerHeight - 25
 
     margin = {
         top: 50,
-        bottom: 80,
-        right: 50,
-        left: 80
+        bottom: 100,
+        right: 30,
+        left: 100
     }
 
     var height = svgHeight - margin.top - margin.bottom
@@ -34,47 +52,119 @@ function makeResponsive() {
     var chartGroup = svg.append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-   d3.csv('assets/csv/state_poverty_vs_healthcare_2014.csv', function(err, data) {
+    // Add Risk/Fact Lable to make it easier to follow the chart
+    svg.append('text')
+        .attr('transform', `translate(20, ${margin.top + 20})`)
+        .attr('font-size', '20px')
+        .text('Risk')
+
+    svg.append('text')
+        .attr('transform', `translate(${width}, ${svgHeight - margin.bottom/2 + 10})`)
+        .attr('font-size', '20px')
+        .text('Fact')
+
+    redraw()
+
+    function redraw() {
+    d3.csv('assets/csv/state_poverty_vs_healthcare_2014.csv', function(err, data) {
+        // Remove axis and dots
         if (err) throw err
+
+        var redraw_objs = ['circle', '.x', '.y', '.title', '.xlabel', '.ylabel']
+        redraw_objs.forEach(d => svg.selectAll(d).remove())
+
+        var fact = FACT
+        var risk = RISK
+
+        svg.append('text')
+            .classed('title', true)
+            .attr('transform', `translate(${width/2 + margin.left}, 20)`)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '20px')
+            .text(`Correlation between ${RISK} vs. ${FACT} by US States`)
 
         // Convert to numeric
         data.forEach( d => {
-            d.poverty = +d.poverty
-            d.healthcare = +d.healthcare
+            d[fact] = +d[fact]
+            d[risk] = +d[risk]
         })
 
-        // X Axis and Label
-        chartGroup.append('g')
-            .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(
-                d3.scaleLinear()
-                    .domain(d3.extent(data, d => d.poverty))
-                    .range([0, width])
-            ))
-         chartGroup.append('text')
-            .attr('transform', `translate(${width/2}, ${height + margin.top/2 + 20})`)
+        // X Label
+        var h = 20;
+        for ( const [fact, label] of Object.entries(FACTS)) {
+          chartGroup.append('text')
+            .classed('xlabel', true)
+            .attr('transform', `translate(${width/2}, ${height + margin.top/2 + h})`)
             .attr('text-anchor', 'middle')
             .attr('font-size', '16px')
-            .text('In Poverty (%)')
+            .attr('fill', function() {
+                if (FACT == fact) return 'black'
+                else return 'gray'
+             })
+            .text(label)
+            .on('click', function() {
+                FACT = fact
+                redraw()
+            })
+         h += 22
+        }
 
-        // Y Axis and Label
-        chartGroup.append('g')
-            .call(d3.axisLeft(
-                d3.scaleLinear()
-                    .domain(d3.extent(data, d => d.healthcare))
-                    .range([height, 0])
-            ))
-         svg.append('text')
-            .attr('transform', `translate(${margin.left / 2}, ${height/2 + margin.top + 20}) rotate(-90)`)
-            .attr('text-anchor', 'start')
+        // X Axis
+       xScale = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d[fact]))
+                    .range([0, width])
+
+       chartGroup.append('g')
+            .classed('x', true)
+            .attr('transform', `translate(0, ${height})`)
+            .call(d3.axisBottom(xScale))
+
+        // Y Labels
+        var w = 20;
+        for ( const [risk, label] of Object.entries(RISKS)) {
+          svg.append('text')
+            .classed('ylabel', true)
+            .attr('transform', `translate(${margin.left / 2 + w}, ${height/2 + 20}) rotate(-90)`)
+            .attr('text-anchor', 'middle')
             .attr('font-size', '16px')
-            .text('Lacks Healthcare (%)')
+            .attr('fill', function() {
+                if (RISK == risk) return 'black'
+                else return 'gray'
+             })
+            .text(label)
+            .on('click', function() {
+                RISK = risk
+                redraw()
+            })
+            w -= 22
+        }
 
+        // Y Axis
+       yScale = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d[risk]))
+                    .range([height, 0])
+       chartGroup.append('g')
+            .classed('y', true)
+            .call(d3.axisLeft(yScale))
 
          // Draw individual data point
+        var circleGroup = chartGroup.selectAll()
+            .data(data)
+            .enter()
+            .append('g')
+            .attr('transform', d => `translate(${xScale(d[fact]) }, ${yScale(d[risk]) })`)
 
+        circleGroup.append('circle')
+            .attr('r', '10')
+            .attr('fill', 'steelblue')
 
+        circleGroup.append('text')
+            .text(d => d.abbr)
+            .classed('state-abbr', true)
+            .attr('x', -7)
+            .attr('y', 3)
          // Add tool tip
-   })
+    })
+    }
 
 }
